39a4216980ff9fcee06e3b19b48779cbb2f50329
Job King <jobezking@gmail.com>
2025-03-08 12:05:42 -0500

    classes

--- learn-python-quickly/chap8.py
+++ learn-python-quickly/chap8.py
@@ -2,8 +2,34 @@ class Employee:         #to create a placeholder class just use declaration with
     pass
 
 class Employee2:
-    def __init__(self, name, email, role): # defines class constructor
-        self.name = name
-        self.email = email
+
+    location = "Lagos, Nigeria"                      # class variable
+
+    def __init__(self, name, email, role="janitor"): # defines class constructor with default role parameter
+        self.name = name                             # assign first parameter
+        self.email = email                           # assign second parameter
         self.role = role
 
+    def getn(self):                                 # Normal methods that do not take parameters
+        return self.name
+    def getr(self):
+        return self.role
+    def gete(self):
+        return self.email
+    def getl(self):
+        return self.location
+    
+    def setrole(cls, newrole):                      #class method that takes parameters. similar to init but uses cls instead of self
+        cls.role = newrole
+
+emails = {'Joe Doe': 'jdoe@x.com', 'Jane Doe': 'doj@y.com', 'Sal Lee': 'sl@u.com', 'Tom Mix': 'tm@c.com'}
+
+employees = []                              # create blank list for employee objets
+for key in emails:                          # iterate through dictionary
+    employee = Employee2(name=key, email=emails.get(key))  # key is name, use key in get() to access paired email address, create object
+    if employee.email == 'sl@u.com':
+        employee.setrole("civil engineer")
+    employees.append(employee)              # add object to list 
+
+for employee in employees:                  # iterate through resulting list
+    print(f"N: {employee.getn()} E: {employee.gete()} R: {employee.getr()} L: {employee.getl()}")
