6ad2ce245f2511e5eda76db1e4457c5682277bad
Job King <jobezking@gmail.com>
2025-03-08 12:07:43 -0500

    variables and casting

--- learn-python-quickly/chap2.py
+++ learn-python-quickly/chap2.py
@@ -3,3 +3,7 @@ num = 3         # declare a number
 num = num * 2   # multiply
 num *= 2        # equal to above
 num = num ** 2  # raise to second power 
+
+now_int = int(9.3)    #cast to int
+now_float = float(9)    #cast to float
+now_str = (9.3)    #cast to string 
