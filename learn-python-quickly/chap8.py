d25301f654119e3cb10d3247888329c1da42d342
Job King <jobezking@gmail.com>
2025-03-08 11:14:08 -0500

    chapter 8

---
+++ learn-python-quickly/chap8.py
@@ -0,0 +1,9 @@
+class Employee:         #to create a placeholder class just use declaration with pass statement
+    pass
+
+class Employee2:
+    def __init__(self, name, email, role): # defines class constructor
+        self.name = name
+        self.email = email
+        self.role = role
+
