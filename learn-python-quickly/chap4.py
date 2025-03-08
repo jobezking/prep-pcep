0397cf1d81adc02b103c01713040ad19e0ce4daa
Job King <jobezking@gmail.com>
2025-03-08 12:10:53 -0500

    lists tuples and dictionaries

--- learn-python-quickly/chap4.py
+++ learn-python-quickly/chap4.py
@@ -37,3 +37,32 @@ d[4] = 'Java'                   #change value using key
 d_empty["first"] = fruits[1]    #add data to dictionary by specifying new key ("first") and new value (fruits[1])
 del d2['b']                     #drop key value pair by key
 d3 = dict(key1=fruits, key2 = "coffee mug", key3 = 9) # create dictionary that contains list
+
+#list and array methods
+'''
+append()	Adds an element at the end of the list
+clear()	Removes all the elements from the list
+copy()	Returns a copy of the list
+count()	Returns the number of elements with the specified value
+extend()	Add the elements of a list (or any iterable), to the end of the current list
+index()	Returns the index of the first element with the specified value
+insert()	Adds an element at the specified position
+pop()	Removes the element at the specified position
+remove()	Removes the first item with the specified value
+reverse()	Reverses the order of the list
+sort()	Sorts the list
+'''
+#dictionary methods
+'''
+clear()	Removes all the elements from the dictionary
+copy()	Returns a copy of the dictionary
+fromkeys()	Returns a dictionary with the specified keys and value
+get()	Returns the value of the specified key
+items()	Returns a list containing a tuple for each key value pair
+keys()	Returns a list containing the dictionary's keys
+pop()	Removes the element with the specified key
+popitem()	Removes the last inserted key-value pair
+setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update()	Updates the dictionary with the specified key-value pairs
+values()	Returns a list of all the values in the dictionary
+'''
