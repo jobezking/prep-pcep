2dfa4d755550e4fd9aeb1b1587a46fdb6b1f8683
Job King <jobezking@gmail.com>
2025-03-13 21:06:06 -0400

    update

---
+++ essentials1/chap6.py
@@ -0,0 +1,13 @@
+print(6./3.) # same as print(6.0/3.0) 
+'''
++  addition
+- subtraction
+* multiplication
+** exponent
+/ divison
+// floor division (integer division, no exponent or remainder, results are always rounded)
+% remainer
+'''
+print(6.1 // 2.9) # answer is 2.0
+print(6//2) # answer is 3
+print(6//4) # answer is 1
---
+++ essentials1/chap7.py
@@ -0,0 +1,16 @@
+'''
+Rules for variables
+0. No restrictions on alphabetic characters. Python treats underscore (_) as an alphabetic character
+1. No special characters (again, note that Python treats underscore as alphabetic character)
+2. cannot contain spaces but - though it is horrible practice - can begin with them
+3. can contain numbers but cannot begin with them
+4. case sensitive 
+
+Shortcut operations with variables
+take any operation:
+variable equals variable and operator and expression
+turn it into 
+variable operator = expression by "dropping" the variable on the other side of the equation
+i.e. x = x + 2 becomes x += 2
+y = x * y - c  becomes y *= x - c
+'''
