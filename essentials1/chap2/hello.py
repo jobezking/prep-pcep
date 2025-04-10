baa50c4bbcb0e3a757e9d420810a3e025b5a34db
Job King <jobezking@gmail.com>
2025-04-10 00:08:21 -0400

    section 2.1

--- essentials1/chap2/hello.py
+++ essentials1/chap2/hello.py
@@ -1,1 +1,15 @@
 print("Hello World Jesus Christ Is Lord and King")
+print("My", "name", "is", sep="_", end="*")
+print("Felix", "Frankfurter.", sep="*", end="*\n")
+print("Felix" *2) #output is FelixFelix
+
+print("My\nname\nis\nBond.",end=" ")
+print("James Bond.")
+
+#print(sep="&", "fish", "chips")  #SyntaxError: positional argument follows keyword argument
+
+print('Greg\'s book.')
+print("'Greg's book.'")
+print('"Greg\'s book."')
+print("Greg\'s book.")
+#print('"Greg's book."') #SyntaxError: unterminated string literal  
