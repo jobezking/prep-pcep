784d4c726d20fe19f466b8db68cea2a5bd259352
Job King <jobezking@gmail.com>
2025-04-11 18:32:38 -0400

    labs

---
+++ essentials1/chap3/lab13.py
@@ -0,0 +1,7 @@
+name = input("Enter flower name: ")
+if name == "Rose":
+    print("Yes, Rose is the best flower!")
+elif name == "rose":
+    print("No, Rose must be capitalized")
+else:
+    print("Rose, not", name + "!")
