e49a91fcb68448d5571b0c806fb87a64432c7140
Job King <jobezking@gmail.com>
2025-04-11 19:25:36 -0400

    lab14.py

---
+++ essentials1/chap3/lab14.py
@@ -0,0 +1,9 @@
+income = float(input("Enter the annual income: "))
+if income < 85528:
+    tax = income * 0.18 - 556.02
+else:
+    tax = (income - 85528) * 0.32 + 14839.02
+if tax < 0.0:
+    tax = 0.0
+
+print("The tax is:", tax, "dollars")
