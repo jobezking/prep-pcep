6f4f830d3cad419fa30455dcbb765fd091d90612
Job King <jobezking@gmail.com>
2025-04-13 21:15:57 -0400

    chap3.py

--- essentials1/chap3/chap3.py
+++ essentials1/chap3/chap3.py
@@ -2,7 +2,7 @@ var = 0
 print(var == 0) # True
 var = 1
 print(var == 1) # True
-numbers = [10, 5, 7, 2, 1]
+numbers = [1, 2, 3, 4, 5]
 ints = [10, 5, 7, 2, 1]
 floats = [10., 5., 7., 2., 1.]
 characs = ['a','b', 'c', 'd', 'e']
@@ -12,3 +12,6 @@ print(numbers[-1])
 print(numbers[-2])
 print(numbers[-3])
 print(numbers[-4])
+print(numbers[-5])
+print(numbers[-len(numbers)])
+print(numbers[-len(numbers) + 1])
